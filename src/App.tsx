b1133adec8ce1df3771c1b0ef81fa62235f92aef
yagnikEancton <yvpanchal@enacton.com>
2025-12-02 17:53:13 +0530

    day 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Counter />
-    </div>
+    </>
   );
 }
 
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,22 +1,36 @@
 import React, { useState } from "react";
 
-function Counter() {
+const Counter = () => {
   const [count, setCount] = useState(0);
-  const handlePlus = () => {
+  const handleIncrement = () => {
     setCount(count + 1);
   };
-  const handleMinus = () => {
+  const handleDecrement = () => {
     if (count === 0) return;
     setCount(count - 1);
   };
+
   return (
-    <div>
-      <h1>Counter: {count}</h1>
-      <button onClick={handlePlus}>Increment</button>
-      &nbsp;
-      <button onClick={handleMinus}>Decrement</button>
+    <div className="flex flex-col justify-center h-screen w-screen border-2 items-center">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-3xl ">Counter: {count}</h1>
+        <div className="flex items-center justify-center mt-10">
+          <button
+            className="bg-sky-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"
+            onClick={handleIncrement}
+          >
+            Increment
+          </button>
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded m-2"
+            onClick={handleDecrement}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Counter;
